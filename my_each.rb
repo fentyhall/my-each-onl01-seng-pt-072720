15c2c93adbb59ac1bedda9e9857313a163bc870c
Fenty Hall <fentymhall@gmail.com>
2020-07-31 02:52:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+    i = 0 
+  
+    while i < collection.length 
+      i += 1  
+      yield(collection[i])
+    end
+    
+    collection 
 end
